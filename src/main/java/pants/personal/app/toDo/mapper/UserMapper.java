af855db59db4b6b6784d67e84af4c0010c7eba22
PanTs <pants.ath@gmail.com>
2026-03-30 20:21:29 +0300

    feat: initialize user mapper

---
+++ src/main/java/pants/personal/app/toDo/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package pants.personal.app.toDo.mapper;
+
+import org.springframework.stereotype.Component;
+import pants.personal.app.toDo.dto.UserCreateDTO;
+import pants.personal.app.toDo.dto.UserReadOnlyDTO;
+import pants.personal.app.toDo.model.User;
+
+@Component
+public class UserMapper {
+
+    public User toEntity(UserCreateDTO dto, String hashedPassword) {
+        User user = new User();
+        user.setEmail(dto.email());
+        user.setUsername(dto.username());
+        user.setPassword(hashedPassword);
+        return user;
+    }
+
+    public UserReadOnlyDTO toReadOnly(User user) {
+        return new UserReadOnlyDTO(
+                user.getUuid(),
+                user.getUsername(),
+                user.getEmail(),
+                user.isActive(),
+                user.getCreatedAt()
+        );
+    }
+}
