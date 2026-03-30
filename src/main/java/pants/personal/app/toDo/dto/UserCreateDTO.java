ce0f203e502ca8f7fe20b5652b978cc636d53583
PanTs <pants.ath@gmail.com>
2026-03-30 20:35:06 +0300

    feat: add validation annotations in dto

--- src/main/java/pants/personal/app/toDo/dto/UserCreateDTO.java
+++ src/main/java/pants/personal/app/toDo/dto/UserCreateDTO.java
@@ -1,6 +1,11 @@
 package pants.personal.app.toDo.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 /**
  * public record for user creation
  * @param username
@@ -8,7 +13,14 @@ package pants.personal.app.toDo.dto;
  * @param password
  */
 public record UserCreateDTO(
+        @NotBlank
         String username,
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
+        @Size(min = 8)
         String password) {
 }
