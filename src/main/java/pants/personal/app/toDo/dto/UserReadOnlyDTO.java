26ce425e4b756ae1c5100f72c1525beb31fd6fa5
PanTs <pants.ath@gmail.com>
2026-03-30 20:13:05 +0300

    chore: refactor to read only

--- src/main/java/pants/personal/app/toDo/dto/UserReadOnlyDTO.java
+++ src/main/java/pants/personal/app/toDo/dto/UserReadOnlyDTO.java
@@ -3,7 +3,7 @@ package pants.personal.app.toDo.dto;
 import java.time.Instant;
 import java.util.UUID;
 
-public record UserResponseDTO(
+public record UserReadOnlyDTO(
         UUID uuid,
         String username,
         String email,
