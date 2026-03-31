e900e04407a78c96bc5346e1117084e91210c29b
PanTs <pants.ath@gmail.com>
2026-03-31 19:20:34 +0300

    chore: refactor documentation

--- src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
+++ src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
@@ -4,9 +4,6 @@ package pants.personal.app.toDo.core.exception;
 /**
  * Exception thrown to indicate that an attempt to create or add an entity has failed
  * because an entity with the same identifier or properties already exists.
- *
- * This exception is typically used in cases where uniqueness constraints are violated
- * during operations such as persisting data to a database or adding elements to a collection.
  */
 public class EntityAlreadyExistsException extends Exception {
     public EntityAlreadyExistsException(String message) {
