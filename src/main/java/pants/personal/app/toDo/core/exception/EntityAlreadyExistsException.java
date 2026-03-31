aa86cc0d7c56782f422a66a52e8c3a4d6b3004a9
PanTs <pants.ath@gmail.com>
2026-03-31 19:30:49 +0300

    fix: update EntityAlreadyExistsException to extend RuntimeException

--- src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
+++ src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
@@ -5,7 +5,7 @@ package pants.personal.app.toDo.core.exception;
  * Exception thrown to indicate that an attempt to create or add an entity has failed
  * because an entity with the same identifier or properties already exists.
  */
-public class EntityAlreadyExistsException extends Exception {
+public class EntityAlreadyExistsException extends RuntimeException {
     public EntityAlreadyExistsException(String message) {
         super(message);
     }
