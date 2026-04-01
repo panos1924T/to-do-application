3972ad483b9739c11c66e993ecf3e3481254d02a
PanTs <pants.ath@gmail.com>
2026-04-01 18:01:19 +0300

    fix: update domain exceptions to extend AppGenericException with specific error codes

--- src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
+++ src/main/java/pants/personal/app/toDo/core/exception/EntityAlreadyExistsException.java
@@ -5,8 +5,8 @@ package pants.personal.app.toDo.core.exception;
  * Exception thrown to indicate that an attempt to create or add an entity has failed
  * because an entity with the same identifier or properties already exists.
  */
-public class EntityAlreadyExistsException extends RuntimeException {
+public class EntityAlreadyExistsException extends AppGenericException {
     public EntityAlreadyExistsException(String message) {
-        super(message);
+        super("ALREADY_EXISTS", message);
     }
 }
--- src/main/java/pants/personal/app/toDo/core/exception/EntityNotFoundException.java
+++ src/main/java/pants/personal/app/toDo/core/exception/EntityNotFoundException.java
@@ -4,8 +4,8 @@ package pants.personal.app.toDo.core.exception;
 /**
  * Exception thrown to indicate that a requested entity could not be found.
  */
-public class EntityNotFoundException extends RuntimeException {
+public class EntityNotFoundException extends AppGenericException {
     public EntityNotFoundException(String message) {
-        super(message);
+        super("NOT_FOUND", message);
     }
 }
