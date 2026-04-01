ff1ab6bcf7602a35a0c33fa4785b2aec66a090ff
PanTs <pants.ath@gmail.com>
2026-04-01 18:10:06 +0300

    feat: enhance ErrorResponseDTO with timestamp, path, and error code fields

--- src/main/java/pants/personal/app/toDo/dto/ErrorResponseDTO.java
+++ src/main/java/pants/personal/app/toDo/dto/ErrorResponseDTO.java
@@ -1,11 +1,16 @@
 package pants.personal.app.toDo.dto;
 
+import java.time.LocalDateTime;
+
 /**
  * A Data Transfer Object (DTO) that encapsulates error response information to be sent
  * to clients in case of exceptions or errors during API interactions.
  */
 public record ErrorResponseDTO(
+        LocalDateTime timestamp,
         int status,
-        String message
+        String code,
+        String message,
+        String path
 ) {
 }
